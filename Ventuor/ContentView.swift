2fcf08ccd7f545eece9efa23136bb6e34d8472e2
H Sam Dean <hgdean@ventuor.com>
2024-01-04 12:04:48 -0500

    no message

---
+++ Ventuor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ventuor
+//
+//  Created by H Sam Dean on 1/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ventuor/VentuorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentuorApp.swift
+//  Ventuor
+//
+//  Created by H Sam Dean on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct VentuorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
