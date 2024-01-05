4836244e03dab39e1214fbd3db19054c305b4e00
H Sam Dean <hgdean@ventuor.com>
2024-01-05 15:42:57 -0500

    Configuration settings for production and develpment

---
+++ Configuration.swift
@@ -0,0 +1,36 @@
+//
+//  Configuration.swift
+//  Ventuor
+//
+//  Created by H Sam Dean on 1/5/24.
+//
+
+import Foundation
+
+enum Configuration {
+    enum Error: Swift.Error {
+        case missingKey, invalidValue
+    }
+
+    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
+            throw Error.missingKey
+        }
+
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            throw Error.invalidValue
+        }
+    }
+}
+
+enum API {
+    static var baseURL: String {
+        return try! "http://" + Configuration.value(for: "BASE_URL")
+    }
+}
